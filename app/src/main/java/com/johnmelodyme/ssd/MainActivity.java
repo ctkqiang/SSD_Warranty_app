3c1f83811548910f00041705705f646ae5ca9432
John Melody Melissa <Johnmelodyme@yandex.com>
2019-11-23 12:31:17 +0800

    upload

--- app/src/main/java/com/johnmelodyme/ssd/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/ssd/MainActivity.java
@@ -1,19 +1,43 @@
 package com.johnmelodyme.ssd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private RecyclerView RV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RV =  findViewById(R.id.recyclerview);
+        new firebaseDataManipulator().readData(new firebaseDataManipulator.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Data> SSD_DATA, List<String> keys) {
+                new recyclerview_config().setConfig(RV, MainActivity.this,
+                        SSD_DATA, keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
     }
 
 }
